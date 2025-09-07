ec1020a2bf6f87d50b18c260c848496c6985818d
Piyush Arora <piyusharora1504@gmail.com>
2025-09-07 21:22:49 +0530

    Home page layout

---
+++ client/src/components/HomeLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar'; 
+
+const HomeLayout = () => {
+    return (
+        <div className="bg-gray-900 text-white">
+            <Navbar />
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    );
+};
+
+export default HomeLayout;
