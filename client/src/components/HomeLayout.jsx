50316df5c196323a645d551da246de528c1c6e2a
Piyush Arora <piyusharora1504@gmail.com>
2025-09-09 21:47:26 +0530

    updated home page layout

--- client/src/components/HomeLayout.jsx
+++ client/src/components/HomeLayout.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from './Navbar'; 
+import Navbar from './Navbar';
 
 const HomeLayout = () => {
     return (
         <div className="bg-gray-900 text-white">
             <Navbar />
-            <main>
+            <main className="relative isolate">
                 <Outlet />
             </main>
         </div>
