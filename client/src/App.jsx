0cd8e7402991c6e660808e6761b93a60667a4698
Piyush Arora <piyusharora1504@gmail.com>
2025-10-01 21:15:52 +0530

    defined routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomeLayout from './components/HomeLayout';
+import ProtectedRoute from './components/ProtectedRoute';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import AuthCallbackPage from './pages/AuthCallbackPage';
 import Dashboard from './pages/Dashboard';
 import ResumeBuilder from './pages/ResumeBuilder';
 import MockInterview from './pages/MockInterview';
@@ -10,15 +14,22 @@ import CoverLetter from './pages/CoverLetter';
 function App() {
   return (
     <Routes>
+      {/* Public Routes */}
       <Route path="/" element={<HomeLayout />}>
         <Route index element={<HomePage />} />
       </Route>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/auth/callback" element={<AuthCallbackPage />} />
 
-      <Route path="/app" element={<Layout />}>
-        <Route index element={<Dashboard />} /> 
-        <Route path="resume-builder" element={<ResumeBuilder />} />
-        <Route path="mock-interview" element={<MockInterview />} />
-        <Route path="cover-letter" element={<CoverLetter />} />
+      {/* Protected App Routes */}
+      <Route element={<ProtectedRoute />}>
+        <Route path="/app" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="resume-builder" element={<ResumeBuilder />} />
+          <Route path="mock-interview" element={<MockInterview />} />
+          <Route path="cover-letter" element={<CoverLetter />} />
+        </Route>
       </Route>
     </Routes>
   );
