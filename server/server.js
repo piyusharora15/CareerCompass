111820d1f487041b0baa5643be52ff34777244e9
Piyush Arora <piyusharora1504@gmail.com>
2025-11-03 21:30:22 +0530

    update

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import 'dotenv/config'; // Handles .env variables
+import passport from 'passport';
+import './config/passport-setup.js'; // Passport configuration
 
 // Import routes
 import usersRouter from './routes/users.js';
@@ -13,6 +15,8 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+app.use(passport.initialize());
 
 // Database Connection
 const uri = process.env.ATLAS_URI;
