a2f0aaf30af66f1a13777b706deb05f8613554f6
Piyush Arora <piyusharora1504@gmail.com>
2025-11-13 21:47:46 +0530

    Home Page Layout

--- client/src/components/HomeLayout.jsx
+++ client/src/components/HomeLayout.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from './Navbar';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const HomeLayout = () => {
-    return (
-        <div className="bg-gray-900 text-white">
-            <Navbar />
-            <main className="relative isolate">
-                <Outlet />
-            </main>
-        </div>
-    );
+  return (
+    <div className="bg-gray-900 text-white">
+      <Navbar />
+      <main className="relative isolate">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default HomeLayout;
