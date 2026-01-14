0c6604b8c9280406c6722815eb6e3594cdc430d9
Piyush Arora <piyusharora1504@gmail.com>
2026-01-14 13:35:22 +0530

    Update Google OAuth callback URL to production

--- server/config/passport-setup.js
+++ server/config/passport-setup.js
@@ -12,7 +12,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/users/google/callback",
+      callbackURL: "https://careercompass-backend-3nf6.onrender.com/users/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
